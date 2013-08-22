6b1b3d98085cbcd9e744f7bd7314b8bf1d102807
Catherine da Graca <cdg.msc@gmail.com>
2013-08-22 20:10:11 +0000

    add stddev mapreduce

---
+++ sumsq_mapper.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import pymongo
+sys.path.append(".")
+
+import traceback
+
+from pymongo_hadoop import BSONMapper
+from pymongo import MongoClient
+
+def mapper(documents):
+  try:
+    connection=MongoClient("cdgmongoserver.chickenkiller.com",27017)
+    db=connection.dialect_db
+    prev_ngram="empty"
+    mean=0
+    for doc in documents:
+     if doc["exclude"] == False:
+        if doc["_id"]["ngram"] != prev_ngram:
+          mean=db.ngram_stats.find_one({"_id": doc["_id"]["ngram"]})["mean"]
+          prev_ngram= doc["_id"]["ngram"]
+        yield {"_id": doc["_id"]["ngram"], "sqr": pow(doc["total_likelihood"]-mean,2)}
+  except:
+    print >> sys.stderr, "Unexpected map error "
+    traceback.print_exc()
+    raise
+
+
+print >> sys.stderr, "Start Mapping"
+try:
+  BSONMapper(mapper)
+except Exception, e:
+  print >> sys.stderr, "something went wrong"
+  traceback.print_exc()
+
---
+++ sumsq_reducer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import pymongo
+sys.path.append(".")
+from pymongo_hadoop import BSONReducer
+from pymongo import MongoClient
+
+def reducer(key, values):
+  try:
+    sum_sqr=0
+    for v in values:
+      sum_sqr+=v["sqr"]
+    return {"_id":key, "sum_squares": sum_sqr, "std_dev": math.sqrt(sum_sqr)}
+  except:
+    print >> sys.stderr, "Unexpected reduce error "
+    traceback.print_exc()
+    raise
+
+print >> sys.stderr, "Start Reduce"
+BSONReducer(reducer)
+print >> sys.stderr, "Done Reduce"
+
