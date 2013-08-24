0ce3122d202075da3cec51362ecf336acc67549c
Catherine da Graca <cdgmsc@gmail.com>
2013-08-24 12:54:07 +0000

    divide. then sqrt.

--- sumsq_reducer.py
+++ sumsq_reducer.py
@@ -11,10 +11,19 @@ from pymongo import MongoClient
 
 def reducer(key, values):
   try:
+    connection=MongoClient("cdgmongoserver.chickenkiller.com",27017)
+    db=connection.dialect_db
+    stats=db.ngram_stats.find_one({"_id":key})
     sum_sqr=0
+    count=0
     for v in values:
       sum_sqr+=v["sqr"]
-    return {"_id":key, "sum_squares": sum_sqr, "std_dev": math.sqrt(sum_sqr)}
+      count+=1
+
+    if count < stats["region_count"]:
+       sum_sqr+=(stats["region_count"]-count) * pow(0-stats["mean"],2)
+       
+    return {"_id":key, "sum_squares": sum_sqr, "std_dev": math.sqrt(sum_sqr/stats["region_count"])}
   except:
     print >> sys.stderr, "Unexpected reduce error "
     traceback.print_exc()
